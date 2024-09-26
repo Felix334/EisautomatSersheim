3c67a445792d3dc83a0b244ddfaadf83ab07e358
Felix334 <felixmayer02@gmail.com>
2024-09-26 18:55:17 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ const getImage = (image_name_) => {
   }
 };
 
+
 const getImageGroß = (image_name_G) => {
   if (image_name_G) {
     return require(`./pictures/${image_name_G}.jpg`);
@@ -17,7 +18,6 @@ const getImageGroß = (image_name_G) => {
     return require(`./pictures/DefaultImage.jpg`);
   }
 };
-
 /*Alle bilder in "Pictures" hochladen und dann die Passenden namen unten Eintragen*/
 /*=>Siehe Beispiel Pistazie/Kaesekuchen!!*/
 
