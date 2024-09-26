ae92d868629c10c1711be38a88a2c2c4732b03e1
Felix334 <felixmayer02@gmail.com>
2024-09-26 19:23:25 +0200

    Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,133 @@ const getImageGroß = (image_name_G) => {
 };
 /*Alle bilder in "Pictures" hochladen und dann die Passenden namen unten Eintragen*/
 /*=>Siehe Beispiel Pistazie/Kaesekuchen!!*/
+
+
+const price = { priceSmall: "3.20", priceBig: "4.80" };
+const Kaesekuchen = {
+  name: "Käsekuchen",
+  img_name_: "Kaesekuchen",
+  available: true,
+};
+const BuntesKleks = {
+  name: "Buntes-Klecks",
+  img_name_: "BuntesKlecksK",
+  image_name_G: "BuntesKlecksG",
+  available: true,
+};
+const Cocoloco = {
+  name: "Cocoloco",
+  img_name_: "DefaultImage",
+  available: true,
+};
+const BananaSplit = {
+  name: "Bananen-Split",
+  img_name_: "DefaultImage",
+  image_name_G: "BananenSplitG",
+  available: true,
+};
+const Haselnuss = {
+  name: "Haselnuss",
+  img_name_: "HaselnussK",
+  image_name_G: "HaselnussG",
+  available: true,
+};
+const Schokolade = {
+  name: "Schokolade",
+  img_name_: "SchokoK",
+  image_name_G: "SchokoG",
+  available: true,
+};
+const Stratcciatella = {
+  name: "Stratcciatella",
+  img_name_: "StracciatellaK",
+  image_name_G: "StratcciatellaG",
+  available: true,
+};
+const JogurtHimbere = {
+  name: "Jogurt-Himbere",
+  img_name_: "DefaultImage",
+  available: true,
+};
+const JogurtCassis = {
+  name: "Jogurt-Cassis",
+  img_name_: "Jogurt-Cassis",
+  image_name_G: "DefaultImage",
+  available: true,
+};
+const Pistazie = {
+  name: "Pistazie",
+  img_name_: "PistazieK",
+  image_name_G: "PistazieG",
+  available: true,
+};
+const JogurtErdbeere = {
+  name: "Jogurt-Erdbeere",
+  img_name_: "Jogurt-Erdbeere",
+  image_name_G: "Jogurt-ErdbeerG",
+  available: true,
+};
+const SaltetKaramell = {
+  name: "Saltet-Karamell",
+  img_name_: "Saltet-Karamell",
+  available: true,
+};
+const Vanille = {
+  name: "Vanille",
+  img_name_: "VanilleK",
+  image_name_G: "VanilleK",
+  available: true,
+};
+const Jogurt = { name: "Jogurt", img_name_: "Jogurt", image_name_G: "JogurtG",available: true };
+const Kaffe = {name: "Kaffe",
+  img_name_: "Kaffe",
+  image_name_G: "DefaultImage",
+  available: true,
+};
+const Kokustraum = {
+  name: "Kokustraum",
+  img_name_: "DefaultImage",
+  available: true,
+};
+const SchokoKeks = {
+  name: "Schoko-Kecks",
+  img_name_: "DefaultImage",
+  image_name_G: "SchokoKeksG",
+  available: true,
+};
+const MangoPassionsFruchtSorbet = {
+  name: "Mango-Passionsfrucht-Sorbet",
+  img_name_: "MangoPassionsfrucht-Sorbet",
+  image_name_G:"Mango-PassionsFrucht-SorbetG",
+  available: true,
+};
+const ErdbeerSorbet = {
+  name: "Erdbeer-Sorbet",
+  img_name_: "DefaultImage",
+  img_name_G: "DefaultImage",
+  available: true,
+};
+const ZitronenSorbet = {
+  name: "Zitronen-Sorbet",
+  img_name_: "DefaultImage",
+  available: true,
+};
+const JogurtHolunder = {
+  name: "Jogurt-Holunder",
+  img_name_: "DefaultImage",
+  available: true,
+};
+const SchokoVegan = {
+  name: "Schoko-Vegan",
+  img_name_: "Schoko-VeganK",
+  available: true,
+};
+const HundeEis = {
+  name: "Hunde-Eis",
+  img_name_: "Hunde-Eis",
+  available: true,
+  special: true,
+};
 const styles = {
   priceStyle: {
     color: "#666",
@@ -252,133 +379,6 @@ const styles = {
     },
   },
 };
-
-const price = { priceSmall: "3.20", priceBig: "4.80" };
-const Kaesekuchen = {
-  name: "Käsekuchen",
-  img_name_: "Kaesekuchen",
-  available: true,
-};
-const BuntesKleks = {
-  name: "Buntes-Klecks",
-  img_name_: "BuntesKlecksK",
-  image_name_G: "BuntesKlecksG",
-  available: true,
-};
-const Cocoloco = {
-  name: "Cocoloco",
-  img_name_: "DefaultImage",
-  available: true,
-};
-const BananaSplit = {
-  name: "Bananen-Split",
-  img_name_: "DefaultImage",
-  image_name_G: "BananenSplitG",
-  available: true,
-};
-const Haselnuss = {
-  name: "Haselnuss",
-  img_name_: "HaselnussK",
-  image_name_G: "HaselnussG",
-  available: true,
-};
-const Schokolade = {
-  name: "Schokolade",
-  img_name_: "SchokoK",
-  image_name_G: "SchokoG",
-  available: true,
-};
-const Stratcciatella = {
-  name: "Stratcciatella",
-  img_name_: "StracciatellaK",
-  image_name_G: "StratcciatellaG",
-  available: true,
-};
-const JogurtHimbere = {
-  name: "Jogurt-Himbere",
-  img_name_: "DefaultImage",
-  available: true,
-};
-const JogurtCassis = {
-  name: "Jogurt-Cassis",
-  img_name_: "Jogurt-Cassis",
-  image_name_G: "DefaultImage",
-  available: true,
-};
-const Pistazie = {
-  name: "Pistazie",
-  img_name_: "PistazieK",
-  image_name_G: "PistazieG",
-  available: true,
-};
-const JogurtErdbeere = {
-  name: "Jogurt-Erdbeere",
-  img_name_: "Jogurt-Erdbeere",
-  image_name_G: "Jogurt-ErdbeerG",
-  available: true,
-};
-const SaltetKaramell = {
-  name: "Saltet-Karamell",
-  img_name_: "Saltet-Karamell",
-  available: true,
-};
-const Vanille = {
-  name: "Vanille",
-  img_name_: "VanilleK",
-  image_name_G: "VanilleK",
-  available: true,
-};
-const Jogurt = { name: "Jogurt", img_name_: "Jogurt", image_name_G: "JogurtG",available: true };
-const Kaffe = {name: "Kaffe",
-  img_name_: "Kaffe",
-  image_name_G: "DefaultImage",
-  available: true,
-};
-const Kokustraum = {
-  name: "Kokustraum",
-  img_name_: "DefaultImage",
-  available: true,
-};
-const SchokoKeks = {
-  name: "Schoko-Kecks",
-  img_name_: "DefaultImage",
-  image_name_G: "SchokoKeksG",
-  available: true,
-};
-const MangoPassionsFruchtSorbet = {
-  name: "Mango-Passionsfrucht-Sorbet",
-  img_name_: "MangoPassionsfrucht-Sorbet",
-  image_name_G:"Mango-PassionsFrucht-SorbetG",
-  available: true,
-};
-const ErdbeerSorbet = {
-  name: "Erdbeer-Sorbet",
-  img_name_: "DefaultImage",
-  img_name_G: "DefaultImage",
-  available: true,
-};
-const ZitronenSorbet = {
-  name: "Zitronen-Sorbet",
-  img_name_: "DefaultImage",
-  available: true,
-};
-const JogurtHolunder = {
-  name: "Jogurt-Holunder",
-  img_name_: "DefaultImage",
-  available: true,
-};
-const SchokoVegan = {
-  name: "Schoko-Vegan",
-  img_name_: "Schoko-VeganK",
-  available: true,
-};
-const HundeEis = {
-  name: "Hunde-Eis",
-  img_name_: "Hunde-Eis",
-  available: true,
-  special: true,
-};
-
 let ice_ = [
   Kaesekuchen,
   BuntesKleks,
