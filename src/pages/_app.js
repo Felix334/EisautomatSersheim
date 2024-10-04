462ecd85c76e26f80f3ec0665042bf4c830be05c
Felix334 <felixmayer02@gmail.com>
2024-10-04 12:27:29 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,11 @@ import "../styles/globals.css";
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
 App.propTypes = {
   Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object.isRequired,
 };
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
