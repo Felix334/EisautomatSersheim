7aefe239df4ec33d8265d51b6c9370b4933df508
Felix334 <felixmayer02@gmail.com>
2024-10-06 12:51:41 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,15 +7,12 @@ import PropTypes from 'prop-types';
 
 // This is the Problem
 
-
-
-
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
 
 App.propTypes = {
   Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object.isRequired,
 };
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
