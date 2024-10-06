91a0b8939d59156614de4eba1920ec83d31e4c3e
Felix334 <felixmayer02@gmail.com>
2024-10-06 16:58:27 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,4 +12,5 @@ export default function App({ Component, pageProps, Analytics }) {
 App.propTypes = {
   Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object.isRequired,
+  Analytics: PropTypes.element.isRequired,
 };
